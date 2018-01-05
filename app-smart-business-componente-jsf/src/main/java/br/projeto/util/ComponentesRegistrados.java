5799f8dd15def89be274a4c5fb03dae414b0bea3
danielmatosdaad <danieladda@icloud.com>
2018-01-05 21:26:02 -0200

    nova versao

--- app-smart-business-componente-jsf/src/main/java/br/projeto/util/ComponentesRegistrados.java
+++ app-smart-business-componente-jsf/src/main/java/br/projeto/util/ComponentesRegistrados.java
@@ -1,21 +1,18 @@
 package br.projeto.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.StringBufferInputStream;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import br.com.projeto.arquivo.util.FileUtil;
+import br.com.projeto.arquivo.util.UnzipUtil;
 import br.projeto.bean.ComponenteBean;
 
 public class ComponentesRegistrados {
 
+	private static final String EXTENSAO_JAR = ".jar";
 	private static final String DIRETORIO_FACELETS = "tags";
 	private static final String EXTENSAO_XHTML = ".xhtml";
 
@@ -31,20 +28,41 @@ public class ComponentesRegistrados {
 	}
 
 	public static List<ComponenteBean> buscarComponentes() {
+		try {
+			// UnzipUtil
+			Path pathComponente = Paths.get(PathResouceFacelet.obterDirectoriResource().toURI());
+			if (pathComponente != null && pathComponente.toString().contains(EXTENSAO_JAR)) {
+				File arquivoComponente = pathComponente.toFile();
+				if (arquivoComponente.isFile()) {
 
-		Path p = FileUtil.buscarDiretorio(Paths.get(PathResouceFacelet.obterDirectoriResource().toURI()),
-				DIRETORIO_FACELETS);
-		System.out.println("Caminho absoluto:" + p.toString());
-		List<File> arquivos = new ArrayList<File>();
-		List<ComponenteBean> componentes = new ArrayList<ComponenteBean>();
-		FileUtil.buscarTodosArquivosDiretorio(p, arquivos, EXTENSAO_XHTML);
-		for (File file : arquivos) {
-			System.out.println("Arquivos encontrados: " + file.getName());
+					//TODO: verificar se pasta ja existe
+					Path pastaDiretorioFacelet = FileUtil.buscarDiretorio(pathComponente.getParent(), DIRETORIO_FACELETS);
+					if(pastaDiretorioFacelet==null){
+						UnzipUtil.unzip(pathComponente.toString(), pathComponente.getParent().toString());
+						
+					}
+					pathComponente = pathComponente.getParent();
+				}
+			}
 
-			ComponenteBean componente = new ComponenteBean(file);
-			componentes.add(componente);
+			Path p = FileUtil.buscarDiretorio(pathComponente, DIRETORIO_FACELETS);
+			System.out.println("Caminho absoluto:" + p.toString());
+			List<File> arquivos = new ArrayList<File>();
+			List<ComponenteBean> componentes = new ArrayList<ComponenteBean>();
+			FileUtil.buscarTodosArquivosDiretorio(p, arquivos, EXTENSAO_XHTML);
+			for (File file : arquivos) {
+				System.out.println("Arquivos encontrados: " + file.getName());
+
+				ComponenteBean componente = new ComponenteBean(file);
+				componentes.add(componente);
+			}
+			return componentes;
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return componentes;
+		return new ArrayList<ComponenteBean>();
 	}
 
 	private static List<File> buscarComponentes(String nomeDiretorio, String extensaoArquivo) {
--- app-smart-business-componente-jsf/src/main/java/br/projeto/util/UiCompositeRegistrado.java
+++ app-smart-business-componente-jsf/src/main/java/br/projeto/util/UiCompositeRegistrado.java
@@ -1,12 +1,6 @@
 package br.projeto.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.StringBufferInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
